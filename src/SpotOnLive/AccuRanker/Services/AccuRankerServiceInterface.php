60b6302941916278b04cdec4f3abe0de15f273bd
Nikolaj Petersen <nikolaj.lovenhardt@gmail.com>
2015-10-14 12:11:44 +0200

    Interface

--- src/SpotOnLive/AccuRanker/Services/AccuRankerServiceInterface.php
+++ src/SpotOnLive/AccuRanker/Services/AccuRankerServiceInterface.php
@@ -4,4 +4,19 @@ namespace SpotOnLive\AccuRanker\Services;
 
 interface AccuRankerServiceInterface
 {
+    /**
+     * List keywords by domain
+     *
+     * @param string $id
+     * @return array
+     */
+    public function listKeywordsForDomain($id);
+
+    /**
+     * Get keyword history
+     *
+     * @param string $keywordId
+     * @return array
+     */
+    public function listKeywordHistory($keywordId);
 }
